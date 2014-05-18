3fabfb90a35fec030447b34a6e05a6f307a028e9
michael <mjdescalzo@yahoo.com>
2014-05-18 22:34:47 +0800

    -start

---
+++ AndroidGame/src/com/mj/framework/Image.java
@@ -0,0 +1,10 @@
+package com.mj.framework;
+
+import com.mj.framework.Graphics.ImageFormat;
+
+public interface Image {
+    public int getWidth();
+    public int getHeight();
+    public ImageFormat getFormat();
+    public void dispose();
+}

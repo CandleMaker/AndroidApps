489944ca5d65bb91ab495ab79a872d773fb21c6c
CandleMaker <mjdescalzo@yahoo.com>
2014-06-01 23:50:53 +0800

    sample

--- MyApp/src/com/mycompany/myapp/MainActivity.java
+++ MyApp/src/com/mycompany/myapp/MainActivity.java
@@ -7,11 +7,17 @@ import android.widget.*;
 
 public class MainActivity extends Activity
 {
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
+		
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
     }
 }
